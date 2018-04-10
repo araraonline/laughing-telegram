087e90c286906d40488f63961304d7aaa9caf4c3
AraraOnline <andre0lra@gmail.com>
2018-04-10 12:17:02 -0300

    Move util function upper

--- src/util.py
+++ src/util.py
@@ -1,4 +1,5 @@
 import pickle
+import re
 
 
 def save_pickle(filepath, obj):
@@ -9,3 +10,15 @@ def save_pickle(filepath, obj):
 def load_pickle(filepath):
     with open(filepath, mode='rb') as f:
         return pickle.load(f)
+
+
+def re_strip(string):
+    """Strips the string using regular expressions
+
+    Args:
+        string: the string to be stripped
+
+    Returns:
+        A new string, without any leading or trailing non-word characters.
+    """
+    return re.search(r'^\W*(.*?)\W*$', string).group(1)
