db6cb57a652efbbcdd28afc2546a851bfa62aefc
AraraOnline <andre0lra@gmail.com>
2018-04-10 17:00:42 -0300

    Refactor betexplorer teams

--- src/data/interim/teams/betexplorer.py
+++ src/data/interim/teams/betexplorer.py
@@ -1,17 +1,11 @@
 import logging
 import re
 import sqlite3
-from collections import defaultdict
 
-import click_log
 import pandas as pd
 
-from src.data.interim.teams.commons import Team, Tokens
-from src.data.interim.teams.util import re_strip
-
-
-logger = logging.getLogger('betexp-teams')
-click_log.basic_config(logger)
+from src.util import re_strip
+from src.data.interim.teams.commons import Team
 
 
 LEAGUE_DICT = {
@@ -38,205 +32,188 @@ LEAGUE_DICT = {
 }
 
 
-def generate_name_to_strings(df):
-    """Generates a BetExplorer team name to BetExplorer team strings mapping
+def parse_string(betexplorer_string):
+    """Parse a BetExplorer team string
 
-    Args:
-        df: the dataframe from which the team names will be retrieved
-
-    Returns:
-        A dictionary that maps (BetExplorer name -> list of BetExplorer strings
-        that generate that name).
-    """
-    dict = defaultdict(list)
-
-    strings = set(df.team_h) | set(df.team_a)
-    for string in strings:
-        name, tokens = extract_tokens(string)
-        dict[name].append(string)
-
-    return dict
-
-
-def guess_state(betexplorer_name, brazilian_df, name_to_strings):
-    """Guess the state for a brazilian team
-
-    This function will look through all the brazilian matches and, if the team
-    played in a match that only happens in a certain state, we will assign this
-    state to the team.
-
-    Args:
-        betexplorer_name: the raw BetExplorer team name (without tokens)
-        brazilian_df: a DataFrame containing all the brazilian matches with the
-            following columns: 'team_h', 'team_a' and 'league_name'
-        name_to_strings: a dict (BetExplorer name -> list of BetExplorer strings
-            that generate that name) for the brazilian teams
-
-    Returns:
-        A two lettered string representing the state of the team. The string is
-        in lowercase. If no state was found, returns None.
-    """
-    df = brazilian_df
-
-    leagues = set()
-    strings = name_to_strings[betexplorer_name]
-    for string in strings:
-        sleagues = set(df[(df.team_h == string) | (df.team_a == string)].league_name)
-        leagues |= sleagues
-
-    states = set([LEAGUE_DICT[league] for league in leagues if league in LEAGUE_DICT])
-
-    if len(states) == 0:
-        return None
-    elif len(states) == 1:
-        return states.pop().lower()
-    else:
-        logger.warning('"%s" found in these states: %s' % (betexplorer_name, sorted(states)))
-        return None
-
-
-def extract_tokens(betexplorer_string):
-    """Extract the tokens from a BetExplorer team string
-
-    Args:
-        betexplorer_string: raw betexplorer team string
-
-    Returns:
-        A tuple (betexplorer_name, tokens) where betexplorer_name is the name of
-        the BetExplorer team without formatting and tokens is an object of type
-        Tokens that contains meta information for the team.
-
-        The Tokens itself contain the following variables:
-            state: the state of the team (always None for this function)
-            country: 3 letter country or None
-            am: True or False (whether the string contains (Am)
-            under: the integer value for UXX or 0
-            women: True or False
+    Check out the return statement.
     """
     str = betexplorer_string
 
-    # women token
-    str, women = re.subn(r'\bW\b', '', str)
-    women = bool(women)
-
-    # am token
+    # flags
     str, am = re.subn(r'\(Am\)', '', str)
-    am = bool(am)
+    str, women = re.subn(r'\bW\b', '', str)
+    am_flag = am > 0
+    women_flag = women > 0
 
-    # under XX token
+    # under XX
     under = re.search(r'\bU(\d{2})\b', str)
     if under:
         under = int(under.group(1))
         str = re.sub(r'\bU\d{2}\b', '', str)
     else:
-        under = 0
+        under = None
 
-    # country token
+    # country
     country = re.search(r'\(([a-zA-Z]{3})\)', str)
     if country:
         country = country.group(1)
-        country = country.lower()
         str = re.sub(r'\(([a-zA-Z]{3})\)', '', str)
     else:
         country = None
 
-    # state token
-    state = None
-
-    # create tokens object
-    tokens = Tokens(state, country, am, under, women)
-
     # the rest is the name of the team
     str = re_strip(str)
 
-    return str, tokens
+    return str, am_flag, women_flag, under, country
 
 
 def format_name(name):
     return name.lower()
 
 
-def generate_string(name, tokens, use_country=False):
-    """Generates a new BetExplorer team string
+def generate_states_dict(conn):
+    """Generates a dict that maps (BE fname -> BE state)
 
-    Args:
-        name: the raw BetExplorer name (without tokens)
-        tokens: a Tokens object
-        use_country: a bool indicating whether we should append the country
-            token to the string
+    If we could not guess the state, it won't be present in the dictionary.
 
-    Returns:
-        A BetExplorer string just like it should be found in the database.
-
-    Note:
-        The "(Am)" token is ignored, as we are not using it.
+    In case there are more than one possible states for a given fname, the
+    function will log an error and set the state to UN.
     """
-    str = name
-    if tokens.under:
-        str += ' U%s' % tokens.under
-    if tokens.women:
-        str += ' W'
-    if use_country and tokens.country:
-        str += ' (%s)' % tokens.country.capitalize()
+    # load data
+    q = """
+        SELECT team_h, team_a, league_name
+        FROM betexp_matches
+        WHERE league_category == 'brazil'
+        """
+    df = pd.read_sql_query(q, conn)
 
-    return str
+    # preprocess dataframe
+    # here we are creating a dataframe with the
+    # following columns: 'fname', 'string' 'league_state'
+    df['league_state'] = df.league_name.apply(lambda x: LEAGUE_DICT.get(x))
+
+    df = pd.concat([
+        df[['team_h', 'league_state']].rename(columns={'team_h': 'string'}),
+        df[['team_a', 'league_state']].rename(columns={'team_a': 'string'})
+    ], axis=0)
+
+    df['fname'] = df.string.apply(lambda x: format_name(parse_string(x)[0]))
+
+    # generate dict
+    # we want each fname to correspond to exactly one state
+    dict = {}
+    for fname, _df in df.groupby('fname'):
+        states = set(_df.league_state) - {None}
+
+        if len(states) == 0:
+            continue
+        elif len(states) == 1:
+            dict[fname] = states.pop()
+        else:
+            msg = "Found multiple states for fname '{}'"
+            msg = msg.format(fname)
+            logging.error(msg)
+            dict[fname] = 'UN'
+
+    return dict
+
+
+def retrieve_out_teams(conn):
+    # retrieve team strings
+    c = conn.cursor()
+
+    c.execute("SELECT team_h FROM betexp_matches WHERE league_category != 'brazil'")
+    teams_h = set([r[0] for r in c.fetchall()])
+
+    c.execute("SELECT team_a FROM betexp_matches WHERE league_category != 'brazil'")
+    teams_a = set([r[0] for r in c.fetchall()])
+
+    c.close()
+    conn.commit()
+
+    strings = teams_h | teams_a
+
+    # generate Team objects
+    teams = []
+    for string in strings:
+        name, am_flag, women_flag, under, country = parse_string(string)
+        fname = format_name(name)
+        team = Team(string, name, fname,
+                    am_flag=am_flag, women_flag=women_flag,
+                    country=country, under=under)
+        teams.append(team)
+
+    return teams
+
+
+def retrieve_brazilian_teams(conn):
+    # retrieve team strings
+    c = conn.cursor()
+    c.execute("SELECT team_h FROM betexp_matches WHERE league_category == 'brazil'")
+    teams_h = set([r[0] for r in c.fetchall()])
+    c.execute("SELECT team_a FROM betexp_matches WHERE league_category == 'brazil'")
+    teams_a = set([r[0] for r in c.fetchall()])
+    c.close()
+    conn.commit()
+
+    strings = teams_h | teams_a
+
+    # generate Team objects
+    teams = []
+    state_dict = generate_states_dict(conn)
+    for string in strings:
+        name, am_flag, women_flag, under, country = parse_string(string)
+        fname = format_name(name)
+        state = state_dict.get(fname)
+        team = Team(string, name, fname, am_flag=am_flag,
+                    women_flag=women_flag, country=country,
+                    state=state, under=under)
+        teams.append(team)
+
+    return teams
 
 
 def retrieve_teams(in_betexp_db):
-    """Retrieves a list of BetExplorer teams
-
-    This will iterate over the BetExplorer database to retrieve the teams and
-    them will process them. Brazilian teams will also contain information about
-    the state they are from.
+    """Retrieve teams from BetExplorer
 
     Args:
-        in_betexp_db: the location of the database to retrieve matches (and
-            consequently teams) from
+        in_betexp_db: The SQLite file where the BetExplorer matches are saved.
 
     Returns:
-        A list of 'Team's, corresponding to the teams that played on the
-        BetExplorer matches. Each Team is an object with the following
-        attributes (from most processed to least processed):
-
-        fname: standardized name of the team (used when comparing from
-            different sources)
-        name: raw name of the team (without tokens)
-        string: raw name of the team (with tokens)
-        tokens: a Tokens object
+        A list of Team objects (commons).
     """
-    teams = []
-
     conn = sqlite3.connect(in_betexp_db)
-
-    # international teams
-    df = pd.read_sql_query("SELECT team_h, team_a FROM betexp_matches WHERE league_category != 'brazil'", conn)
-    strings = set(df.team_h) | set(df.team_a)
-    strings = sorted(strings)
-
-    for string in strings:
-        name, tokens = extract_tokens(string)
-        fname = format_name(name)
-        team = Team(fname, name, string, tokens)
-        teams.append(team)
-
-    # brazilian teams
-    df = pd.read_sql_query("SELECT team_h, team_a, league_name FROM betexp_matches WHERE league_category == 'brazil'", conn)
-    name_to_strings = generate_name_to_strings(df)
-    strings = set(df.team_h) | set(df.team_a)
-    strings = sorted(strings)
-
-    for string in strings:
-        name, tokens = extract_tokens(string)
-        fname = format_name(name)
-
-        # guess state
-        state = guess_state(name, df, name_to_strings)
-        tokens = tokens._replace(state=state)
-
-        # save
-        team = Team(fname, name, string, tokens)
-        teams.append(team)
-
+    out_teams = retrieve_out_teams(conn)
+    brazilian_teams = retrieve_brazilian_teams(conn)
     conn.close()
 
-    return teams
+    return out_teams + brazilian_teams
+
+
+def generate_string(team, use_country=False):
+    """Generates a new BetExplorer team string
+
+    Args:
+        team: A Team object (commons). Its name must correspond to the desired
+            BetExplorer name.
+        use_country: Whether the country abbreviation must appear in the
+            generated string.
+
+    Returns:
+        A BetExplorer string like it should be found in the database.
+
+    Note:
+        The "(Am)" token is ignored.
+    """
+    raise NotImplementedError  # check the use of this function
+
+    str = team.name
+
+    if team.under:
+        str += ' U%s' % team.under
+    if team.women_flag:
+        str += ' W'
+    if use_country and team.country:
+        str += ' (%s)' % team.country.capitalize()
+
+    return str
